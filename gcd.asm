8ef97bb2c931690ae14ead38d4f5b611728bbf09
Stephen Kil <stephenkil@cslab20.wheaton.edu>
2021-04-30 23:54:32 -0500

    prints 18 to 24

--- gcd.asm
+++ gcd.asm
@@ -1,1 +1,19 @@
-// Put your code here!
+movi 18 r1 // i = 18 (uses r1 as i)
+
+top:
+add r17 r1
+out r1 //print i
+
+movi 1 r2 //r2 = 1
+add r2 r1 //r1 += 1 (basically i++)
+
+mov r1 r3 //r3 = r1
+movi 25 r2 //r2 = 25
+sub r2 r3 // r3 =- r2
+
+movi top r2 //r2 = top
+jnz r3 r2 //if(r2!=0) jumps to r2
+
+gcd:
+
+halt
